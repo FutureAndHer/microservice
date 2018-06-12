6779456afb23472455074a10c27a7cf6694f642b
dorayo <yomingyo@gmail.com>
2018-06-13 00:03:35 +0800

    在eureka上注册服务

--- microservice-consumer-movie/src/main/java/com/vi/cloud/microservicesimpleconsumermovie/MicroserviceConsumerMovieApplication.java
+++ microservice-consumer-movie/src/main/java/com/vi/cloud/microservicesimpleconsumermovie/MicroserviceConsumerMovieApplication.java
@@ -2,11 +2,13 @@ package com.vi.cloud.microservicesimpleconsumermovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class MicroserviceSimpleConsumerMovieApplication {
+@EnableEurekaClient
+public class MicroserviceConsumerMovieApplication {
 
     @Bean
     public RestTemplate restTemplate() {
@@ -14,6 +16,6 @@ public class MicroserviceSimpleConsumerMovieApplication {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroserviceSimpleConsumerMovieApplication.class, args);
+        SpringApplication.run(MicroserviceConsumerMovieApplication.class, args);
     }
 }
