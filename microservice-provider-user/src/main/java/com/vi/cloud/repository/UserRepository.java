ef42cdda938981733cc1dece9ac833805d8fc16e
dorayo <yomingyo@gmail.com>
2018-06-19 11:35:18 +0800

    eureka注册中心

--- microservice-provider-user/src/main/java/com/vi/cloud/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vi.cloud.repository;
-
-import com.vi.cloud.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
